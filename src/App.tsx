5418223af8be75ea9bffdefe4587f442b97301b8
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-01-19 12:31:31 +0600

    Created components for user form, users list and user

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { useState } from 'react';
+import UserForm from './components/UserForm/UserForm';
+import Users from './components/Users/Users';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <div className='container pt-5'>
+      <div className='row'>
+        <UserForm />
+        <Users />
+      </div>
+    </div>
+  );
 }
 
 export default App;
